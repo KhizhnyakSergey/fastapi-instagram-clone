8b10401a600ddafdf87399e122271fae0fc0ad7b
KhizhnyakSergey <Khihnyak56843@gmail.com>
2022-08-08 14:44:26 +0300

    feat: added method which shows all posts, added uploading files to a static folder

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 from db import models
 from db.database import engine
-from routers import user
+from routers import user, post
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(post.router)
 
 @app.get("/")
 def root():
     return 'Hello world'
 
 models.Base.metadata.create_all(engine)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
